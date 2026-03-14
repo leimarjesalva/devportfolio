26e5fbdf022a1128e1df38e766868e49b56e3654
Leimar Jesalva <ljesalva1@gmail.com>
2026-03-14 00:43:01 +0000

    fix allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: process.env.PORT || 4173,
     host: true,
+    port: process.env.PORT || 4173,
+    allowedHosts: ['mydevportfolio.up.railway.app'],
   }
-})  
+})
